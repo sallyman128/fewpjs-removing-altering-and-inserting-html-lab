83d86b56e994eff54aaca565bdf1816187568c08
Salmaan <salmaan.arif.ali@gmail.com>
2021-06-13 15:15:06 -0400

    commit tree 311e395b74dff813085016db216d5d723bf4d963

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'Salmaan is the champion';
